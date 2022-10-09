e156eb78abb3aff32fb6bc9232df445d414d08a0
Richard Clamp <richardc@unixbeard.net>
2022-10-09 21:44:17 +0100

    Day 3 Part 2: Check in vertical chunks of 3
    Each chunk of 3 lines makes 3 triangles, taking across the verticals.
    
    There's probably a cleaner way to organise this.

--- src/day03.rs
+++ src/day03.rs
@@ -18,3 +18,36 @@ fn solve(input: &str) -> usize {
 fn test_solve() {
     assert_eq!(solve("5 10 25"), 0);
 }
+
+#[aoc(day3, part2)]
+fn solve2(input: &str) -> usize {
+    input
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .flat_map(|c| {
+            let square = c
+                .map(|l| {
+                    l.split_ascii_whitespace()
+                        .map(|v| v.parse::<i32>().unwrap())
+                        .collect_vec()
+                })
+                .collect_vec();
+            vec![
+                vec![square[0][0], square[1][0], square[2][0]],
+                vec![square[0][1], square[1][1], square[2][1]],
+                vec![square[0][2], square[1][2], square[2][2]],
+            ]
+        })
+        .map(|mut t| {
+            t.sort();
+            t
+        })
+        .filter(|t| t[0] + t[1] > t[2])
+        .count()
+}
+
+#[test]
+fn test_solve2() {
+    assert_eq!(solve2(include_str!("day03_example.txt")), 6);
+}
