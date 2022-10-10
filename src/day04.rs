45bb68ef045b2ad07f202b94550b533ec441c77c
Richard Clamp <richardc@unixbeard.net>
2022-10-10 12:10:34 +0100

    Day 4 Part 2: Decrypt room names
    Minor fight with the type system as to rotNNN you need to take a char,
    make it a number, add another number, mod 26, add another number, turn
    it back into a char.

--- src/day04.rs
+++ src/day04.rs
@@ -49,6 +49,30 @@ fn test_room_checksum() {
     )
 }
 
+impl Room {
+    fn decrypt(&self) -> String {
+        self.name
+            .chars()
+            .map(|c| match c {
+                '-' => ' ',
+                _ => unsafe {
+                    char::from_u32_unchecked(
+                        ((c as u32 - b'a' as u32 + self.sector) % 26) + b'a' as u32,
+                    )
+                },
+            })
+            .collect()
+    }
+}
+
+#[test]
+fn test_room_decrypt() {
+    assert_eq!(
+        &Room::from("qzmt-zixmtkozy-ivhz-343[aaaaa]").decrypt(),
+        "very encrypted name"
+    )
+}
+
 #[aoc_generator(day4)]
 fn generate(input: &str) -> Vec<Room> {
     input.lines().map(Room::from).collect()
@@ -57,4 +81,13 @@ fn generate(input: &str) -> Vec<Room> {
 #[aoc(day4, part1)]
 fn solve(rooms: &[Room]) -> u32 {
     rooms.iter().filter(|r| r.legal()).map(|r| r.sector).sum()
+}
+
+#[aoc(day4, part2)]
+fn solve2(rooms: &[Room]) -> u32 {
+    rooms
+        .iter()
+        .filter(|r| r.legal() && r.decrypt() == "northpole object storage")
+        .map(|r| r.sector)
+        .sum()
 }
