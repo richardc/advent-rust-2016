39591f732188cab2d5b030d22877fa97e7ced86b
Richard Clamp <richardc@unixbeard.net>
2022-10-10 15:49:02 +0100

    Day 6 Part 2: Sort the other way
    Parameterise a sort function and pass it in from two wrappers.

--- src/day06.rs
+++ src/day06.rs
@@ -2,8 +2,7 @@ use std::collections::HashMap;
 
 use itertools::Itertools;
 
-#[aoc(day6, part1)]
-fn solve(input: &str) -> String {
+fn solver(input: &str, sort: fn(&usize, &usize) -> std::cmp::Ordering) -> String {
     let lines = input.lines();
     let mut counts: Vec<HashMap<u8, usize>> = vec![];
     for l in lines {
@@ -19,7 +18,7 @@ fn solve(input: &str) -> String {
     }
     String::from_iter(counts.into_iter().map(|c| {
         c.iter()
-            .sorted_by(|(_, av), (_, bv)| Ord::cmp(bv, av))
+            .sorted_by(|(_, av), (_, bv)| sort(av, bv))
             .map(|(&k, _)| k as char)
             .take(1)
             .next()
@@ -27,7 +26,22 @@ fn solve(input: &str) -> String {
     }))
 }
 
+#[aoc(day6, part1)]
+fn solve(input: &str) -> String {
+    solver(input, |av, bv| Ord::cmp(bv, av))
+}
+
 #[test]
 fn test_solve() {
     assert_eq!(solve(include_str!("day06_example.txt")), "easter");
+}
+
+#[aoc(day6, part2)]
+fn solve2(input: &str) -> String {
+    solver(input, |av, bv| Ord::cmp(av, bv))
+}
+
+#[test]
+fn test_solve2() {
+    assert_eq!(solve2(include_str!("day06_example.txt")), "advent");
 }
