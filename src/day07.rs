fdeeec2632b5f536ea902715e1aa188faee7d70b
Richard Clamp <richardc@unixbeard.net>
2022-10-10 17:22:20 +0100

    Day 07 Part 2: SSL address decoding
    Extract all the BAB strings from the Network chunks, and return them as
    a vector of strings in form ABA, then the has_ssl uses those on the
    address parts.

--- src/day07.rs
+++ src/day07.rs
@@ -3,6 +3,11 @@ fn solve(input: &str) -> usize {
     input.lines().filter(|l| has_tls(l)).count()
 }
 
+#[aoc(day7, part2)]
+fn solve2(input: &str) -> usize {
+    input.lines().filter(|l| has_ssl(l)).count()
+}
+
 fn parse_addr(addr: &str) -> (Vec<&str>, Vec<&str>) {
     let mut addrs = vec![];
     let mut nets = vec![];
@@ -39,4 +44,38 @@ fn test_has_tls() {
     assert!(!has_tls("aaaa[qwer]tyui"));
     assert!(has_tls("ioxxoj[asdfgh]zxcvbn"));
     assert!(!has_tls("abba[mnop]qrst[abba]foos"));
+}
+
+fn net_aba(addr: &str) -> Vec<String> {
+    let mut found = vec![];
+    let b = addr.as_bytes();
+    for i in 0..b.len() - 2 {
+        if b[i] == b[i + 2] && b[i] != b[i + 1] {
+            // We found an aba, record the corresponding bab
+            found.push(String::from_iter(vec![
+                b[i + 1] as char,
+                b[i] as char,
+                b[i + 1] as char,
+            ]));
+        }
+    }
+    found
+}
+
+fn has_ssl(addr: &str) -> bool {
+    let (addrs, nets) = parse_addr(addr);
+    for aba in nets.into_iter().flat_map(net_aba) {
+        if addrs.iter().any(|s| s.contains(&aba)) {
+            return true;
+        }
+    }
+    false
+}
+
+#[test]
+fn test_has_ssl() {
+    assert!(has_ssl("aba[bab]xyz"));
+    assert!(!has_ssl("xyx[xyx]xyx"));
+    assert!(has_ssl("aaa[kek]eke"));
+    assert!(has_ssl("zazbz[bzb]cdb"));
 }
