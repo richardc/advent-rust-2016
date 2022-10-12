fb555c62f02b2b380864b968029219622f1f061e
Richard Clamp <richardc@unixbeard.net>
2022-10-12 15:58:41 +0100

    Day 13 Part 1: Dijkstra over a computed maze
    We just need to code up is_wall then feed that into Dijkstra with
    weights 1.

---
+++ src/day13.rs
@@ -0,0 +1,47 @@
+use itertools::Itertools;
+use pathfinding::prelude::dijkstra;
+
+fn is_wall(seed: i32, x: i32, y: i32) -> bool {
+    if x < 0 || y < 0 {
+        true
+    } else {
+        (x * x + 3 * x + 2 * x * y + y + y * y + seed).count_ones() % 2 == 1
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn test_is_wall() {
+    assert!(is_wall(10, -1, 0), "Outside is walls");
+    assert!(!is_wall(10, 0, 0));
+    assert!(is_wall(10, 1, 0));
+}
+
+fn find_route(seed: i32, dx: i32, dy: i32) -> usize {
+    let goal = (dx, dy);
+    let (_path, cost) = dijkstra(
+        &(1, 1),
+        |(x, y)| {
+            [(-1, 0), (1, 0), (0, 1), (0, -1)]
+                .iter()
+                .map(|(xp, yp)| (x + xp, y + yp))
+                .filter(|&(x, y)| !is_wall(seed, x, y))
+                .map(|p| (p, 1))
+                .collect_vec()
+        },
+        |&p| p == goal,
+    )
+    .unwrap();
+    cost
+}
+
+#[cfg(test)]
+#[test]
+fn test_find_route() {
+    assert_eq!(find_route(10, 7, 4), 11);
+}
+
+#[aoc(day13, part1)]
+fn solve(input: &str) -> usize {
+    find_route(input.parse().unwrap(), 31, 39)
+}
