35cc392445740003a4eb5ae26d7abbd2a0326b3f
Richard Clamp <richardc@unixbeard.net>
2022-10-12 20:45:46 +0100

    Day 14 Part 2: Hash even harder
    It seems we're just trying to burn my lap.

--- src/day14.rs
+++ src/day14.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use memoize::memoize;
 
 fn three_in_a_row(hash: &[u8]) -> Option<u8> {
@@ -24,13 +23,34 @@ fn five_in_a_row(needle: u8, hash: &[u8]) -> bool {
     false
 }
 
-#[memoize(Capacity: 1000)]
-fn hasher(salt: String, index: usize) -> String {
-    format!("{:x}", md5::compute(format!("{}{}", salt, index)))
+fn inner_hasher(input: String) -> String {
+    format!("{:x}", md5::compute(input))
 }
 
-#[aoc(day14, part1)]
-fn solve(salt: &str) -> usize {
+#[memoize(Capacity: 1001)]
+fn stretching_hasher(salt: String, index: usize) -> String {
+    let mut hash = single_hasher(salt, index);
+    for _ in 0..2016 {
+        hash = inner_hasher(hash);
+    }
+    hash
+}
+
+#[cfg(test)]
+#[test]
+fn test_stretching_hasher() {
+    assert_eq!(
+        stretching_hasher("abc".to_string(), 0),
+        "a107ff634856bb300138cac6568c0f24"
+    );
+}
+
+#[memoize(Capacity: 1001)]
+fn single_hasher(salt: String, index: usize) -> String {
+    inner_hasher(format!("{}{}", salt, index))
+}
+
+fn solver(salt: &str, hasher: fn(String, usize) -> String) -> usize {
     let mut index = 0;
     for _digit in 0..64 {
         'checker: loop {
@@ -55,8 +75,24 @@ fn solve(salt: &str) -> usize {
     index - 1
 }
 
+#[aoc(day14, part1)]
+fn solve(salt: &str) -> usize {
+    solver(salt, single_hasher)
+}
+
 #[cfg(test)]
 #[test]
 fn test_solve() {
     assert_eq!(solve("abc"), 22728)
+}
+
+#[aoc(day14, part2)]
+fn solve2(salt: &str) -> usize {
+    solver(salt, stretching_hasher)
+}
+
+#[cfg(test)]
+#[test]
+fn test_solve2() {
+    assert_eq!(solve2("abc"), 22859)
 }
