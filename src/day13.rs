60c8f121df6873b8fa700ed3a1e7fb9f4767c772
Richard Clamp <richardc@unixbeard.net>
2022-10-12 16:31:19 +0100

    Day 13 Part 2: Just do something dumb/inefficient
    Check if we can path to everywhere in the 50x50 grid, keeping the nodes
    where we can get there in <= 50 moves, and then uniqify and count them.
    
    Dumb, but computers are fast:
    
        AOC 2016
        Day 13 - Part 1 : 86
                generator: 3.825µs,
                runner: 125.02µs
    
        Day 13 - Part 2 : 127
                generator: 425ns,
                runner: 237.455344ms

--- src/day13.rs
+++ src/day13.rs
@@ -1,3 +1,4 @@
+use itertools::iproduct;
 use itertools::Itertools;
 use pathfinding::prelude::dijkstra;
 
@@ -17,21 +18,19 @@ fn test_is_wall() {
     assert!(is_wall(10, 1, 0));
 }
 
+fn successors(seed: i32, x: i32, y: i32) -> Vec<((i32, i32), usize)> {
+    [(-1, 0), (1, 0), (0, 1), (0, -1)]
+        .iter()
+        .map(|(xp, yp)| (x + xp, y + yp))
+        .filter(|&(x, y)| !is_wall(seed, x, y))
+        .map(|p| (p, 1))
+        .collect_vec()
+}
+
 fn find_route(seed: i32, dx: i32, dy: i32) -> usize {
     let goal = (dx, dy);
-    let (_path, cost) = dijkstra(
-        &(1, 1),
-        |(x, y)| {
-            [(-1, 0), (1, 0), (0, 1), (0, -1)]
-                .iter()
-                .map(|(xp, yp)| (x + xp, y + yp))
-                .filter(|&(x, y)| !is_wall(seed, x, y))
-                .map(|p| (p, 1))
-                .collect_vec()
-        },
-        |&p| p == goal,
-    )
-    .unwrap();
+    let (_path, cost) =
+        dijkstra(&(1, 1), |&(x, y)| successors(seed, x, y), |&p| p == goal).unwrap();
     cost
 }
 
@@ -44,4 +43,18 @@ fn test_find_route() {
 #[aoc(day13, part1)]
 fn solve(input: &str) -> usize {
     find_route(input.parse().unwrap(), 31, 39)
+}
+
+#[aoc(day13, part2)]
+fn solve2(input: &str) -> usize {
+    let seed = input.parse().unwrap();
+    iproduct!(0..=50, 0..=50)
+        .flat_map(|goal| {
+            match dijkstra(&(1, 1), |&(x, y)| successors(seed, x, y), |&p| p == goal) {
+                Some((nodes, count)) if count <= 50 => nodes,
+                _ => vec![],
+            }
+        })
+        .unique()
+        .count()
 }
