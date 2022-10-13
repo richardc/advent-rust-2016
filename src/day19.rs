69a04067a4eef73dfdcef81005b6334be0eec519
Richard Clamp <richardc@unixbeard.net>
2022-10-14 00:18:22 +0100

    Day 19 Part 1: Josephus is a very naughty elf
    I owe all this to the Numberphile video on the Josephus problem.
    
    I'm indulging in a little light microbenchmarking to see if the version
    that slices and makes new strings is faster/slower that the version that
    rotates the bytes in place.  It's a wash when running from the CLI:
    
      AOC 2016
      Day 19 - Part 1 - rotate : 1815603
              generator: 5.493µs,
              runner: 12.378µs
    
      Day 19 - Part 1 - slice : 1815603
              generator: 407ns,
              runner: 11.36µs
    
    Criterion hands it to the rotate version, with ~260ns vs ~550ns, which
    makes sense as it avoids an allocation.  Both seem acceptably fast for
    our purposes though.

---
+++ src/day19.rs
@@ -0,0 +1,30 @@
+#[aoc_generator(day19)]
+fn generate(input: &str) -> usize {
+    input.parse().unwrap()
+}
+
+#[aoc(day19, part1, slice)]
+fn josephus(input: &usize) -> usize {
+    // First find: 2^m + l for the highest power of 2
+    // Answer: is 2 * l + 1.
+
+    // Numberphile's video shows a neat trick with the binary representation,
+    // first make the value binary, then stringwise rotate it, such that MSB
+    // becomes the LSB.
+    let bits = format!("{:b}", *input);
+    let rotated = format!("{}1", &bits[1..]);
+    usize::from_str_radix(&rotated, 2).unwrap()
+}
+
+#[aoc(day19, part1, rotate)]
+fn josephus_fiddling(input: &usize) -> usize {
+    let mut bits = format!("{:b}", *input);
+    unsafe { bits.as_bytes_mut() }.rotate_left(1);
+    usize::from_str_radix(&bits, 2).unwrap()
+}
+
+#[cfg(test)]
+#[test]
+fn test_josephus() {
+    assert_eq!(josephus(&5), 3);
+}
