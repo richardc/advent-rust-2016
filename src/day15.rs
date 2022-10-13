fc5a5e81f2a212a58e3a72d60555a5c8ba28f909
Richard Clamp <richardc@unixbeard.net>
2022-10-13 11:18:47 +0100

    Day 15 Part 1: Find the time where all the holes line up
      AOC 2016
      Day 15 - Part 1 : 148737
              generator: 723.551µs,
              runner: 1.95482ms

---
+++ src/day15.rs
@@ -0,0 +1,67 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+struct Disc {
+    start: usize,
+    period: usize,
+}
+
+#[derive(Debug)]
+struct DiscParseErr {}
+impl FromStr for Disc {
+    type Err = DiscParseErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref RE: Regex =
+                Regex::new(r"has (\d+) positions; at time=0, it is at position (\d+)\.").unwrap();
+        }
+        let captures = RE.captures(s).unwrap();
+        let period = captures.get(1).unwrap().as_str().parse().unwrap();
+        let start = captures.get(2).unwrap().as_str().parse().unwrap();
+        Ok(Disc { start, period })
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn test_disc_from_str() {
+    let disc: Disc = "Disc #1 has 5 positions; at time=0, it is at position 4."
+        .parse()
+        .unwrap();
+
+    assert_eq!(
+        disc,
+        Disc {
+            start: 4,
+            period: 5
+        }
+    )
+}
+
+#[aoc_generator(day15)]
+fn generate(s: &str) -> Vec<Disc> {
+    s.lines().map(|l| l.parse().unwrap()).collect()
+}
+
+#[aoc(day15, part1)]
+fn solve(discs: &[Disc]) -> usize {
+    'time: for time in 0.. {
+        for (i, disc) in discs.iter().enumerate() {
+            if (i + 1 + disc.start + time) % disc.period != 0 {
+                continue 'time;
+            }
+        }
+        return time;
+    }
+    unreachable!()
+}
+
+#[cfg(test)]
+#[test]
+fn test_solve() {
+    assert_eq!(solve(&generate(include_str!("day15_example.txt"))), 5)
+}
