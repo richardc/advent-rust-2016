2542921ed15656f2941d01124c6100d9fb14866c
Richard Clamp <richardc@unixbeard.net>
2022-10-14 01:34:49 +0100

    Day 19 Part 2: More maths
    Inspired by the numberphile video on how to do the analysis, we did one.
    Took a while to spot the pattern.
    
     P  | W
     ------
     1  | 1
     2  | 1
     3  | 2
     4  | 1
     5  | 2
     6  | 3
     7  | 4
     8  | 5
     9  | 6
    10  | 1
    
    It's advancing in ones, and resets just after a power of 3.
    
    This guided guessing to get us to W = P - 3^m, which fits the general
    pattern of the Josephus formula, and the aoc site confirms our answer.

--- src/day19.rs
+++ src/day19.rs
@@ -33,3 +33,65 @@ fn josephus_bitmath(input: &usize) -> usize {
 fn test_josephus() {
     assert_eq!(josephus(&5), 3);
 }
+
+#[aoc(day19, part2)]
+fn joe2(players: &usize) -> usize {
+    // 0 and 1 player games are basecases
+    if *players < 2 {
+        return *players;
+    }
+    // Analysis shows W = P - 3^m
+    let mut power_of_three = 1;
+    while power_of_three * 3 < *players {
+        power_of_three *= 3
+    }
+    *players - power_of_three
+}
+
+// This idiom to emulate table-based tests is growing on me a bit - as distinct
+// functions they run as seperate threads, plus with some sympathetic naming you
+// can see the case at a glance.
+#[cfg(test)]
+mod joe2 {
+    use super::*;
+    #[test]
+    fn t_1() {
+        assert_eq!(joe2(&1), 1);
+    }
+    #[test]
+    fn t_2() {
+        assert_eq!(joe2(&2), 1);
+    }
+    #[test]
+    fn t_3() {
+        assert_eq!(joe2(&3), 2);
+    }
+    #[test]
+    fn t_4() {
+        assert_eq!(joe2(&4), 1);
+    }
+    #[test]
+    fn t_5() {
+        assert_eq!(joe2(&5), 2);
+    }
+    #[test]
+    fn t_6() {
+        assert_eq!(joe2(&6), 3);
+    }
+    #[test]
+    fn t_7() {
+        assert_eq!(joe2(&7), 4);
+    }
+    #[test]
+    fn t_8() {
+        assert_eq!(joe2(&8), 5);
+    }
+    #[test]
+    fn t_9() {
+        assert_eq!(joe2(&9), 6);
+    }
+    #[test]
+    fn t_10() {
+        assert_eq!(joe2(&10), 1);
+    }
+}
